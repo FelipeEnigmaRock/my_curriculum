61ca6562c80d32a40da4703eedfb486fbe0b54ae
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-06-17 22:51:22 -0300

    Create main.js

---
+++ curriculo escrito/main.js
@@ -0,0 +1,42 @@
+
+function Curricullum () {
+    const dadosCandidato = {
+        nome: 'Felipe Pereira Gomes da Silva',
+        localidade: 'Aracaju-SE, Brazil',
+        telefone: +55 (79) 99800-9919,
+        email: "felj.gomes@gmail.com"
+    };
+    
+    function aboutMe (description) {
+        description = [
+        'I'm a web developer, Front End
+        I really like using Vue.js in
+        my projects, and I'm currently
+        focused on learning also about Back-End.'
+        ];
+    };
+    
+    function professionalExperiences () {
+        const firstJob = {
+            modelo: 'Freelancer',
+            task: 'Criar um site de apresentação para Construtora Leite,
+                   foi feito uma landing page', 
+        }
+        const projects = 'Prática de vários eventos que participei e aulas
+                          que assisti e fiz seus projetos para assimilar
+                          melhor o conteúdo'
+    };
+    
+    function educationData (leanguages, education, certifications ) {
+        leanguages = ['Advanced English', 'Native Portuguese', 'Basic Spanish'];
+        education = 'Bachelor of Accountancy (UNIVERSIDADE TIRADENTES -UNIT 2014–2019)';
+        certifications = [
+            Bootcamps: 'https://github.com/FelipeEnigmaRock/bootcamps/tree/main/Certificados!',
+            Udemy: 'Curso de JavaScript e TypeScript do básico ao avançado 2022'
+        ];
+ };
+
+    function portifólio () {
+        const gitHub = 'https://github.com/FelipeEnigmaRock'
+    }
+}
